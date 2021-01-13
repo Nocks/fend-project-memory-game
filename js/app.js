273ce8d408058a61fee50936619a020a11b3d742
Enock Kwesi Addey <enock.addey@gmail.com>
2021-01-13 19:50:44 +0000

    Add core function of game

--- js/app.js
+++ js/app.js
@@ -37,13 +37,71 @@ function shuffle(array) {
      deck.appendChild(currentCard);
  }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+window.addEventListener('load', (event) => {
+    let movesCounterElement = document.querySelector('.moves');
+    movesCounterElement.textContent = 0;
+});
+
+let openCards = [];
+let movesCounter = 0;
+let matchedCards = 0;
+let win = false;
+
+function declareWin() {
+    window.alert("You've finally matched all cards with " + movesCounter + " moves");
+    win = false;
+}
+
+ deck.addEventListener('click', function(event) {
+
+     function updateMoveCounter() {
+         movesCounter += 1;
+         let movesCounterElement = document.querySelector('.moves');
+         movesCounterElement.textContent = movesCounter;
+     }
+
+     function updateMatches() {
+         matchedCards += 1;
+         if (matchedCards === shuffledCards.length / 2) {
+             win = true;
+         }
+     }
+
+     function showCardSymbol(evt) {
+        evt.target.classList.add("open", "show");
+        addOpenCard(event);
+        updateMoveCounter();
+     }
+
+     function lockCards(firstCard, secondCard) {
+         firstCard.classList.add("match");
+         secondCard.classList.add("match");
+         openCards = [];
+         updateMatches();
+     }
+
+     function hideCards(firstCard, secondCard) {
+         openCards = [];
+         firstCard.classList.remove("open", "show");
+         secondCard.classList.remove("open", "show");
+     }
+
+     function addOpenCard(evt) {
+         openCards.push(evt.target);
+     }
+
+     if (!event.target.classList.contains("open", "show")) {
+         showCardSymbol(event);
+     }
+    //  Check if two cards are the same
+     if (openCards.length === 2) {
+         if (openCards[0].childNodes[1].classList[1] === openCards[1].childNodes[1].classList[1]) {
+             lockCards(openCards[0], openCards[1]);
+         } else {
+             hideCards(openCards[0], openCards[1]);
+         }
+     }
+     if (win) {
+         declareWin();
+     }
+ });
